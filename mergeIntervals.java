578a2e8d3d67c3d3eb801c0fa227d8348768a320
Shivam kumar <swadeshi9936@gmail.com>
2023-07-26 00:17:33 +0530

    merge Intervals solution

---
+++ mergeIntervals.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class mergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        ArrayList<int[]>res=new ArrayList<>();
+        for(int i=0;i<intervals.length;i++){
+            if(res.size()==0||intervals[i][0]>res.get(res.size()-1)[1]){
+                res.add(intervals[i]);
+            }else{
+                res.get(res.size()-1)[1] = Math.max(res.get(res.size()-1)[1], intervals[i][1]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
